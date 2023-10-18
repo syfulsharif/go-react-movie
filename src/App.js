5e18cc0141c13478c551c99cc5966b9b82f726cb
Syful Sharif <sharif.syful@gmail.com>
2023-10-18 21:47:59 +0600

    Homepage Created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return <div>Go Watch a Movie!</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go Watch a Movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!">
+            <span className="badge bg-success">Login</span>
+          </a>
+        </div>
+      </div>
+      <hr className="md-3"></hr>
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">
+                Home
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Movies
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <>
+            <div className="text-center">
+                <h2>Find a movie to watch tonight</h2>
+            </div>
+        </>
+    )
+}
