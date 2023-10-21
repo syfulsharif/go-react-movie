d5d1efe4704c1f7b03b71a4f0c634fab0ac2d408
Syful Sharif <sharif.syful@gmail.com>
2023-10-21 19:24:52 +0600

    Individual Movie Page Created showing data

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Movie = () => {
-    return (
-        <div className="text-center">
-          <h2>Movie</h2>
-          <hr></hr>
-        </div>
-    );
-  };
-  
-  export default Movie;
-  
+  const [movie, setMovie] = useState({});
+  let { id } = useParams();
+
+  useEffect(() => {
+    let myMovie = {
+      id: 3,
+      title: "Old Dads",
+      release_date: "2023-10-23",
+      runtime: 110,
+      mpaa_rating: "R",
+      description: "Some long description",
+    };
+    setMovie(myMovie);
+  }, [id]);
+
+  return (
+    <div>
+      <h2>Movie: {movie.title}</h2>
+      <small>
+        <em>
+          {movie.release_date}, {movie.runtime} minutes, Rated{" "}
+          {movie.mpaa_rating}
+        </em>
+      </small>
+      <hr></hr>
+      <p>{movie.description}</p>
+    </div>
+  );
+};
+
+export default Movie;
